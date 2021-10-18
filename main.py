94fc2992daba164e2d3edc0caecc55a0aa6d8755
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-10-19 00:10:04 +0500

    деши влево, вправо и вниз
    короткие рывки в различных направлениях

--- main.py
+++ main.py
@@ -1,53 +1,8 @@
 import pygame
-from math import *
+import time
 window = pygame.display.set_mode((500, 500))
-
-
-class Rouge:
-    # характеристики класса
-    def __init__(self):
-        mass = 5
-        health = 80
-        damage = 200
-        texture = pygame.draw.rect(window, (255, 255, 255), (20, 20, 20, 20))
-        move_speed = 15
-
-
-class Warrior:
-    # характеристики класса
-    def __init__(self):
-        mass = 15
-        health = 150
-        damage = 80
-        texture = ""
-        move_speed = 10
-        Fp = mass * 100
-
-
-class Druid:
-    # характеристики класса
-    def __init__(self):
-        mass = 5
-        health = 100
-        damage = 50
-        texture = ""
-        move_speed = 10
-        Fp = mass * 100
-
-
-class Monk:
-    # характеристики класса
-        move_speed = 30
-        mass = 7
-        health = 100
-        damage1 = 120
-        damage2 = 50
-        x = 20
-        y = 480
-
-        def Move(self, x, y):
-            pygame.draw.rect(window, (255, 255, 255), (x, y, 20, 20))
-
+timer = pygame.time.Clock()
+timer.tick(60)
 
 class Ranger:
     # характеристики класса
@@ -57,7 +12,7 @@ class Ranger:
     damage1 = 120
     damage2 = 50
     x = 20
-    y = 480
+    y = 450
     jump_count = 10
     jump = False
 
@@ -65,22 +20,25 @@ class Ranger:
     def Move(self):
         # Захват кнопок и текстурка
         keys = pygame.key.get_pressed()
-        pygame.draw.rect(window, (255, 255, 255), (self.x, self.y, 20, 20))
-
+        pygame.draw.rect(window, (255, 255, 255), (self.x, self.y, 30, 30))
         # проверка нажатия
-        if keys[pygame.K_d] and self.x <= 470:
+        if keys[pygame.K_d] and self.x <= 460:
             self.x += self.move_speed
-        if keys[pygame.K_a] and self.x >= 10:
-            self.x -= self.move_speed
-        if keys[pygame.K_w]:
-            self.jump = True
-        if keys[pygame.K_s] and self.y <= 490:
-            self.jump = False
-            self.jump_count = 10
-            self.y += self.mass
 
-        # прыжок
-        if self.jump and self.y<=490:
+        elif keys[pygame.K_a] and self.x >= 10:
+            self.x -= self.move_speed
+
+        elif keys[pygame.K_w]:
+            self.jump = True
+
+        elif keys[pygame.K_s]:
+            while self.y<450:
+                self.jump = False
+                self.jump_count = 10
+                self.y += self.mass
+
+            # прыжок
+        if self.jump:
             if self.jump_count >= -10:
                 self.y -= (self.jump_count ** 3) / self.mass
                 self.jump_count -= 1
@@ -88,6 +46,23 @@ class Ranger:
                 self.jump_count = 10
                 self.jump = False
 
+        for event in pygame.event.get():
+            if event.type == pygame.KEYUP:
+                if keys[pygame.K_e]:
+                    self.x += self.move_speed * 10
+                    if self.x > 470:
+                        self.x = 470
+
+                if keys[pygame.K_q]:
+                    self.x -= self.move_speed * 10
+                    if self.x < 0:
+                        self.x = 0
+
+            elif event.type == pygame.QUIT:
+                exit()
+
+        if self.y > 450:
+            self.y = 450
 
 
 pygame.init()
@@ -95,14 +70,11 @@ pygame.display.set_caption('castle rush')
 player = Ranger
 
 while True:
+
     window.fill((0, 0, 0))
     player.Move(player)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            break
-
     pygame.display.update()
-    pygame.time.delay(25)
+    pygame.time.delay(20)
 
 
